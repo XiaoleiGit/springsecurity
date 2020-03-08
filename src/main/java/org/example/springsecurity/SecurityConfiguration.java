bfb15bc201d66bf9a7f9a4db83f2845a9712ecd2
Xiaolei <winny_wxl@hotmail.com>
2020-03-08 23:07:49 +0100

    Add authentication

---
+++ src/main/java/org/example/springsecurity/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package org.example.springsecurity;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //Set in-memory authentication
+        auth.inMemoryAuthentication()
+                .withUser("foo")
+                .password("foo")
+                .roles("USER");
+    }
+
+}
