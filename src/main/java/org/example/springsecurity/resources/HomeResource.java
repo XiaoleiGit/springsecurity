c3ae35f7907915fb2e9c9d17e76e9c250f57276d
Xiaolei <winny_wxl@hotmail.com>
2020-03-08 22:50:16 +0100

    first commit

---
+++ src/main/java/org/example/springsecurity/resources/HomeResource.java
@@ -0,0 +1,13 @@
+package org.example.springsecurity.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome";
+    }
+}
