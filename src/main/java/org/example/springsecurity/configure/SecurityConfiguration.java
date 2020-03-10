2e03886a3a5fed77f8b20594548f8632b8161ac3
Xiaolei <winny_wxl@hotmail.com>
2020-03-10 22:16:10 +0100

    Setup JDBC authentication with Spring Security

--- src/main/java/org/example/springsecurity/configure/SecurityConfiguration.java
+++ src/main/java/org/example/springsecurity/configure/SecurityConfiguration.java
@@ -1,26 +1,38 @@
 package org.example.springsecurity.configure;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //Set in-memory authentication
-        auth.inMemoryAuthentication()
-                .withUser("foo")
-                .password("foo")
-                .roles("USER")
-                .and()
-                .withUser("wang")
-                .password("wang")
-                .roles("ADMIN");
+//        Set in-memory authentication
+//        auth.inMemoryAuthentication()
+//                .withUser("foo")
+//                .password("foo")
+//                .roles("USER")
+//                .and()
+//                .withUser("wang")
+//                .password("wang")
+//                .roles("ADMIN");
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
+
 
     }
 
@@ -33,9 +45,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/user").hasRole("USER")
-                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/admin").hasAnyRole("ADMIN","USER")
                 .antMatchers("/").permitAll()
                 .and().formLogin();
     }
+
+
 
 }
